092007b77485fa61517dd2e276369549727aab85
rsteube <rsteube@users.noreply.github.com>
2020-10-29 12:49:25 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,145 @@
+use std::collections::HashMap;
+use std::env;
+use std::process;
+use sysinfo::{ProcessExt, System, SystemExt};
+
+fn main() {
+    match parent_process_name().as_str() {
+        "bash" => println!("{}", bash(completers())),
+        "elvish" => println!("{}", elvish(completers())),
+        "fish" => println!("{}", fish(completers())),
+        "pwsh" => println!("powershell shell"),
+        p if p.starts_with("python") => println!("{}", xonsh(completers())),
+        "zsh" => println!("{}", zsh(completers())),
+        p => println!("unknown shell: {}", p),
+    }
+}
+
+fn completers() -> HashMap<String, String> {
+    let mut completers = HashMap::new();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() % 2 == 0 {
+        panic!("invalid amount of arguments")
+    }
+
+    for n in 1..args.len() {
+        if n % 2 == 1 {
+            completers.insert(args[n].to_string(), args[n + 1].to_string());
+        }
+    }
+    return completers;
+}
+
+fn bash(c: HashMap<String, String>) -> String {
+    let mut elems: Vec<String> = Vec::new();
+    for (k, v) in &c {
+        elems.append(&mut vec![format!(
+            "_lazycomplete_{}() {{
+  unset -f _lazycomplete_{}
+  source <({})
+  $\"$(complete -p {} | awk '{{print $3}}')\"
+}}
+complete -F _lazycomplete_{} {}
+",
+            k, k, v, k, k, k
+        )]);
+    }
+
+    return elems.join("\n");
+}
+
+fn elvish(c: HashMap<String, String>) -> String {
+    let mut elems: Vec<String> = Vec::new();
+    for (k, v) in &c {
+        elems.append(&mut vec![format!(
+            "edit:completion:arg-completer[{}] = [@arg]{{}}
+        edit:completion:arg-completer[{}] = [@arg]{{}}
+        eval ({} | slurp)
+        $edit:completion:arg-completer[{}] $@arg
+    }}
+}}
+",
+            k, k, v, k
+        )]);
+    }
+
+    return elems.join("\n");
+}
+
+fn fish(c: HashMap<String, String>) -> String {
+    let mut elems: Vec<String> = Vec::new();
+    for (k, v) in &c {
+        elems.append(&mut vec![format!(
+            "function _lazycomplete_{}
+   functions --erase _lazycomplete_{}
+   complete -c '{}' --erase
+   {} | source
+   complete --do-complete=(commandline -cp)
+end
+complete -c '{}' --erase
+complete -c '{}' -f -a '(_lazycomplete_{} {})'
+",
+            k, k, k, v, k, k, k, k
+        )]);
+    }
+
+    return elems.join("\n");
+}
+
+fn xonsh(c: HashMap<String, String>) -> String {
+    let mut elems: Vec<String> = Vec::new();
+    for (k, v) in &c {
+        elems.append(&mut vec![format!(
+            "def _lazycomplete_{}(prefix, line, begidx, endidx, ctx):
+    \"\"\"lazycomplete {}\"\"\"
+    import xonsh
+    import subprocess
+    import builtins
+    if not line.startswith('{} '):
+        return # not the expected command to complete
+    del _lazycomplete_{} 
+    builtins.__xonsh__.completers = builtins.__xonsh__.completers.copy()
+    del builtins.__xonsh__.completers['{}']
+    exec(compile(subprocess.run(['{}'], stdout=subprocess.PIPE).stdout.decode('utf-8'), \"\", \"exec\"))
+    return builtins.__xonsh__.completers['{}'](prefix, line, begidx, endidx, ctx)
+completer add {} _lazycomplete_{}
+",
+            k, k,k, k, k, v, k, k, k
+        )]);
+    }
+
+    return elems.join("\n");
+}
+
+fn zsh(c: HashMap<String, String>) -> String {
+    let mut elems: Vec<String> = Vec::new();
+    for (k, v) in &c {
+        elems.append(&mut vec![format!(
+            "function _lazycomplete_{} {{
+    compdef -d {}
+    unfunction _lazycomplete_{}
+    source <({})
+}}
+compdef _lazycomplete_{} {}
+",
+            k, k, k, v, k, k
+        )]);
+    }
+
+    return elems.join("\n");
+}
+
+fn parent_process_name() -> String {
+    let mut system = System::new();
+    let parent_pid: i32;
+    {
+        system.refresh_process(process::id() as i32);
+        let current = system.get_process(process::id() as i32).unwrap();
+        parent_pid = current.parent().unwrap();
+    }
+    system.refresh_process(parent_pid as i32);
+
+    let parent = system.get_process(parent_pid).unwrap();
+    parent.name().to_string()
+}
